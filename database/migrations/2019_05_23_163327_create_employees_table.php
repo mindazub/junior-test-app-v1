b47c138b19b2ef9135454a4352f139e8526423b7
Mindaugas Azubalis <mindaugas.azubalis@gmail.com>
2019-05-23 17:12:54 +0000

    employee and company models and migrations, factories added. Migrations tweaked, others - empty. TODO

---
+++ database/migrations/2019_05_23_163327_create_employees_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEmployeesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('firstName');
+            $table->string('lastName');
+            $table->unsignedInteger('company_id')->nullable();
+            $table->string('email_employee')->unique()->nullable();
+            $table->integer('phone')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('employees');
+    }
+}
