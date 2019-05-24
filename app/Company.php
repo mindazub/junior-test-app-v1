543cf639fa44d6dd39d15b1b44fa0130f63ebec0
Mindaugas Azubalis <mindaugas.azubalis@gmail.com>
2019-05-24 09:54:29 +0000

    admin model and migration added and migrated

--- app/Company.php
+++ app/Company.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Company extends Model
 {
-    //
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'email', 'logo',
+    ];
+
 }
--- app/Employee.php
+++ app/Employee.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Employee extends Model
 {
-    //
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'firstName', 'lastName', 'company_id', 'email', 'phone',
+    ];
+
 }
