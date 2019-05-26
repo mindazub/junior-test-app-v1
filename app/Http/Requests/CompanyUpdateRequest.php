fc33d8aeae4876b0d4db2d2a3ed9669b95f65472
Mindaugas Azubalis <mindaugas.azubalis@gmail.com>
2019-05-26 13:12:55 +0000

    CompanyUpdateRequest tweaked and working fine.

--- app/Http/Requests/CompanyUpdateRequest.php
+++ app/Http/Requests/CompanyUpdateRequest.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Requests;
 
+use Illuminate\Validation\Rule;
 
 class CompanyUpdateRequest extends CompanyStoreRequest
 {
@@ -25,7 +26,7 @@ class CompanyUpdateRequest extends CompanyStoreRequest
         return [
             'name'=>'required|min:5|max:25|string',
             'website' => 'required|min:3|max:191|string',
-            'email' => 'required|email|unique:companies',
+            'email' => 'required|email',
             'logo' => 'nullable|image|min:1|max:2048|dimensions:min_width:100,max_width:3000',
         ];
     }
