b09c13a84fdfb0e9f6276310a0d919ba93279f2f
Mindaugas Azubalis <mindaugas.azubalis@gmail.com>
2019-05-30 08:29:26 +0000

    tests edited and couple others are added

--- tests/Feature/LoginTest.php
+++ tests/Feature/LoginTest.php
@@ -19,22 +19,39 @@ class LoginTest extends TestCase
     }
 
     /** @test */
-    public function a_user_can_see_all_the_employees()
+    public function test_register_page_not_found()
     {
-        $employee = factory('App\Employee')->create();
-
-        $response = $this->get('/employee');
-
-        $response->assertSee($employee->firstName);
+        $response = $this->get('/register');
+        $response->assertNotFound();
+        $response->assertStatus(404);
     }
 
     /** @test */
-    public function a_user_can_see_all_the_companies()
+    public function test_an_authenticated_user_can_see_employees()
     {
+        $this->be($user = factory('App\User')->create([
+            'email' => 'admin@admin.com',
+            'password' => bcrypt('password'),
+        ]));
+
+        $employee = factory('App\Employee')->create();
+        $response = $this->get('/employee');
+        $response->assertSee($employee->name);
+    }
+
+
+
+    /** @test */
+    public function test_an_authenticated_user_can_see_companies()
+    {
+        $this->be($user = factory('App\User')->create([
+            'email' => 'admin@admin.com',
+            'password' => bcrypt('password'),
+        ]));
+
         $company = factory('App\Company')->create();
-
         $response = $this->get('/company');
-
         $response->assertSee($company->name);
     }
+
 }
