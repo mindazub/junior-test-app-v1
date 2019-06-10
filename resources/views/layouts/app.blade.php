b8449d1623760ac8710813f1cd5765ba24fd6e2d
Mindaugas Azubalis <mindaugas.azubalis@gmail.com>
2019-06-10 12:35:46 +0000

    adminlte2 on the front end, login works ok

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,92 +1,175 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
+<html style="height: auto; min-height: 100%;"><head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>JuniorLTE 2 | by mindazub</title>
+    <!-- Tell the browser to be responsive to screen width -->
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+    <!-- Bootstrap 3.3.7 -->
+    <link rel="stylesheet" href="../../bower_components/bootstrap/dist/css/bootstrap.min.css">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="../../bower_components/font-awesome/css/font-awesome.min.css">
+    <!-- Ionicons -->
+    <link rel="stylesheet" href="../../bower_components/Ionicons/css/ionicons.min.css">
+    <!-- Theme style -->
+    <link rel="stylesheet" href="../../dist/css/AdminLTE.min.css">
+    <!-- AdminLTE Skins. Choose a skin from the css/skins
+         folder instead of downloading all of them to reduce the load. -->
+    <link rel="stylesheet" href="../../dist/css/skins/_all-skins.min.css">
 
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <!-- Google Font -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
+    <style type="text/css">.backpack.dropzone {
+            font-family: 'SF UI Display', 'Segoe UI';
+            font-size: 15px;
+            text-align: center;
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            width: 250px;
+            height: 150px;
+            font-weight: lighter;
+            color: white;
+            will-change: right;
+            z-index: 2147483647;
+            bottom: 20%;
+            background: #333;
+            position: fixed;
+            user-select: none;
+            transition: left .5s, right .5s;
+            right: 0px; }
+        .backpack.dropzone .animation {
+            height: 80px;
+            width: 250px;
+            background: url("chrome-extension://lifbcibllhkdhoafpjfnlhfpfgnpldfl/assets/backpack/dropzone/hoverstate.png") left center; }
+        .backpack.dropzone .title::before {
+            content: 'Save to'; }
+        .backpack.dropzone.closed {
+            right: -250px; }
+        .backpack.dropzone.hover .animation {
+            animation: sxt-play-anim-hover 0.91s steps(21);
+            animation-fill-mode: forwards;
+            background: url("chrome-extension://lifbcibllhkdhoafpjfnlhfpfgnpldfl/assets/backpack/dropzone/hoverstate.png") left center; }
 
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
+        @keyframes sxt-play-anim-hover {
+            from {
+                background-position: 0px; }
+            to {
+                background-position: -5250px; } }
+        .backpack.dropzone.saving .title::before {
+            content: 'Saving to'; }
+        .backpack.dropzone.saving .animation {
+            background: url("chrome-extension://lifbcibllhkdhoafpjfnlhfpfgnpldfl/assets/backpack/dropzone/saving_loop.png") left center;
+            animation: sxt-play-anim-saving steps(59) 2.46s infinite; }
 
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+        @keyframes sxt-play-anim-saving {
+            100% {
+                background-position: -14750px; } }
+        .backpack.dropzone.saved .title::before {
+            content: 'Saved to'; }
+        .backpack.dropzone.saved .animation {
+            background: url("chrome-extension://lifbcibllhkdhoafpjfnlhfpfgnpldfl/assets/backpack/dropzone/saved.png") left center;
+            animation: sxt-play-anim-saved steps(20) 0.83s forwards; }
 
-    <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-</head>
-<body>
-    <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+        @keyframes sxt-play-anim-saved {
+            100% {
+                background-position: -5000px; } }
+    </style></head>
+<!-- ADD THE CLASS layout-top-nav TO REMOVE THE SIDEBAR. -->
+<body class="skin-blue layout-top-nav" style="height: auto; min-height: 100%;">
+<div class="wrapper" style="height: auto; min-height: 100%;">
+
+    <header class="main-header">
+        <nav class="navbar navbar-static-top">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
+                <div class="navbar-header">
+                    <a href="{{ url('/') }}" class="navbar-brand"><b>Junior</b>LTE</a>
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse">
+                        <i class="fa fa-bars"></i>
+                    </button>
+                </div>
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
+                <!-- Collect the nav links, forms, and other content for toggling -->
 
-                    @guest
-                    <ul class="navbar-nav mr-auto">
+                <!-- Navbar Right Menu -->
+                <div class="navbar-custom-menu">
+                    <ul class="nav navbar-nav">
 
-                    </ul>
-                    @else
-                        <ul class="navbar-nav mr-auto">
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('home') }}">{{ __('Companies') }}</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('employee.index') }}">{{ __('Employees') }}</a>
-                            </li>
-                        </ul>
-                    @endguest
 
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                            </li>
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
-
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
+                        <!-- User Account Menu -->
+                        <li class="dropdown user user-menu">
+                            <!-- Menu Toggle Button -->
+                            <a href="{{ route('login') }}" >
+                                <!-- The user image in the navbar-->
+                                <img src="/dist/img/usermale.png" class="user-image" alt="User Image">
+                                <!-- hidden-xs hides the username on small devices so only the image appears. -->
+                                <span class="hidden-xs">Login</span>
+                            </a>
+                        </li>
                     </ul>
                 </div>
+                <!-- /.navbar-custom-menu -->
             </div>
+            <!-- /.container-fluid -->
         </nav>
+    </header>
+    <!-- Full Width Column -->
+    <div class="content-wrapper" style="min-height: 284px;">
+        <div class="container">
+            <!-- Section Content Header (Page header) -->
 
-        <main class="py-4">
-            @yield('content')
-        </main>
+
+            <!-- Main content -->
+            <section class="content">
+
+                <div class="box box-default">
+
+                    <div class="box-body">
+
+                        @yield('content')
+
+                    </div>
+                    <!-- /.box-body -->
+                </div>
+                <!-- /.box -->
+            </section>
+            <!-- /.content -->
+        </div>
+        <!-- /.container -->
     </div>
-</body>
-</html>
+    <!-- /.content-wrapper -->
+    <footer class="main-footer">
+        <div class="container">
+            <div class="pull-right hidden-xs">
+                <b>Version</b> beta-0.0.1
+            </div>
+            <strong>Copyright © 2019 <a href="http://www.azubalis.lt">mindazub</a>.</strong> All rights
+            reserved.
+        </div>
+        <!-- /.container -->
+    </footer>
+</div>
+<!-- ./wrapper -->
+
+<!-- jQuery 3 -->
+<script async="" src="//www.google-analytics.com/analytics.js"></script><script src="../../bower_components/jquery/dist/jquery.min.js"></script>
+<!-- Bootstrap 3.3.7 -->
+<script src="../../bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+<!-- SlimScroll -->
+<script src="../../bower_components/jquery-slimscroll/jquery.slimscroll.min.js"></script>
+<!-- FastClick -->
+<script src="../../bower_components/fastclick/lib/fastclick.js"></script>
+<!-- AdminLTE App -->
+<script src="../../dist/js/adminlte.min.js"></script>
+<!-- AdminLTE for demo purposes -->
+<script src="../../dist/js/demo.js"></script>
+
+
+</body></html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,146 +1,63 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.app')
 
-        <title>Laravel</title>
+@section('content')
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    <div class="flex-center position-ref full-height">
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-            ul {
-                padding: 10px;
-                margin: 10px;
-                text-align: left;
-            }
-            p {
-                text-align: left;
-            }
-
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Junior Laravel App:
-                </div>
-                <div class="title m-b-md">
-                    Companies and Employees CRUD
-                </div>
-                <div class="m-b-md">
-                    <ul>
-                        <li>
-                            Basic Laravel Auth: ability to log in as administrator
-                        </li>
-                        <li>
-                            Use database seeds to create first user with email admin@admin.com and password “password”
-                        </li>
-                        <li>
-                            CRUD functionality (Create / Read / Update / Delete) for two menu items: Companies and Employees.
-                        </li>
-                        <li>
-                            Companies DB table consists of these fields: Name (required), email, logo (minimum 100×100), website
-                        </li>
-                        <li>
-                            Employees DB table consists of these fields: First name (required), last name (required), Company (foreign key to Companies), email, phone
-                        </li>
-                        <li>
-                            Use database migrations to create those schemas above
-                        </li>
-                        <li>
-                            Store companies logos in storage/app/public folder and make them accessible from public
-                        </li>
-                        <li>
-                            Use basic Laravel resource controllers with default methods – index, create, store etc.
-                        </li>
-                        <li>
-                            Use Laravel’s validation function, using Request classes
-                        </li>
-                        <li>
-                            Use Laravel’s pagination for showing Companies/Employees list, 10 entries per page
-                        </li>
-                        <li>
-                            Use Laravel make:auth as default Bootstrap-based design theme, but remove ability to register
-                        </li>
-                    </ul>
-                    <p>
-                        Source:
-                        <a href="https://laraveldaily.com/test-junior-laravel-developer-sample-project/?fbclid=IwAR16D8PoHaf84zsLvMiAwSdcTqq_5B2aH3_koL2c9pzaARkQdW9Ods5nMwQ">
-                            How to Test Junior Laravel Developer Skills: Sample Project
-                        </a>
-                    </p>
-                    
-                </div>
+        <div class="content">
+            <div class="title m-b-md">
+                Junior Laravel App:
+            </div>
+            <div class="title m-b-md">
+                Companies and Employees CRUD
+            </div>
+            <div class="m-b-md">
+                <ul>
+                    <li>
+                        Basic Laravel Auth: ability to log in as administrator
+                    </li>
+                    <li>
+                        Use database seeds to create first user with email admin@admin.com and password “password”
+                    </li>
+                    <li>
+                        CRUD functionality (Create / Read / Update / Delete) for two menu items: Companies and Employees.
+                    </li>
+                    <li>
+                        Companies DB table consists of these fields: Name (required), email, logo (minimum 100×100), website
+                    </li>
+                    <li>
+                        Employees DB table consists of these fields: First name (required), last name (required), Company (foreign key to Companies), email, phone
+                    </li>
+                    <li>
+                        Use database migrations to create those schemas above
+                    </li>
+                    <li>
+                        Store companies logos in storage/app/public folder and make them accessible from public
+                    </li>
+                    <li>
+                        Use basic Laravel resource controllers with default methods – index, create, store etc.
+                    </li>
+                    <li>
+                        Use Laravel’s validation function, using Request classes
+                    </li>
+                    <li>
+                        Use Laravel’s pagination for showing Companies/Employees list, 10 entries per page
+                    </li>
+                    <li>
+                        Use Laravel make:auth as default Bootstrap-based design theme, but remove ability to register
+                    </li>
+                </ul>
+                <p>
+                    Source:
+                    <a href="https://laraveldaily.com/test-junior-laravel-developer-sample-project/?fbclid=IwAR16D8PoHaf84zsLvMiAwSdcTqq_5B2aH3_koL2c9pzaARkQdW9Ods5nMwQ">
+                        How to Test Junior Laravel Developer Skills: Sample Project
+                    </a>
+                </p>
 
             </div>
+
         </div>
-    </body>
-</html>
+    </div>
+
+
+@endsection
