e1f24129e2e8c94a13d0f71b1915f1bdc139270e
Mindaugas Azubalis <mindaugas.azubalis@gmail.com>
2019-06-20 09:43:25 +0000

    redirect to en/dashboard added

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -25,7 +25,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/en/dashboard';
 
     /**
      * Create a new controller instance.
@@ -41,6 +41,6 @@ class LoginController extends Controller
      * @return string
      */
     public function redirectTo() {
-        return app()->getLocale() .'/home';
+        return app()->getLocale() .'/dashboard';
     }
 }
