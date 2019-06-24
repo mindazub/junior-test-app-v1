ab5275509b397eb5cb55b3c669a9108d8592650e
Mindaugas Azubalis <mindaugas.azubalis@gmail.com>
2019-06-24 14:35:33 +0000

    login form changed to a new one

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,69 +1,69 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login', app()->getLocale()) }}">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="email" class="col-lg-12 col-md-12 col-sm-12 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-lg-12 col-md-12 col-sm-10">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-lg-12 col-md-12 col-sm-12 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-lg-12 col-md-12 col-sm-10">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
+    <div class="login-box">
+        <div class="login-logo">
+            <a href="#"><b>Admin</b>{{ __('Login') }}</a>
         </div>
+        <!-- /.login-logo -->
+        <div class="login-box-body">
+
+            <form method="POST" action="{{ route('login', app()->getLocale()) }}">
+                @csrf
+                <div class="form-group has-feedback">
+                    <input id="email" type="email"
+                           class="form-control @error('email') is-invalid @enderror"
+                           name="email" value="{{ old('email') }}"
+                           required autocomplete="email"
+                           placeholder="Email"
+                           autofocus
+                    >
+                    @error('email')
+                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                    @enderror
+                </div>
+
+                <div class="form-group has-feedback">
+                    <input id="password"
+                           type="password"
+                           class="form-control @error('password') is-invalid @enderror"
+                           name="password"
+                           required
+                           autocomplete="current-password"
+                    >
+
+                    @error('password')
+                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                    @enderror
+                </div>
+                <div class="row">
+                    <div class="col-xs-8">
+                        <div class="form-check">
+                            <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+
+                            <label class="form-check-label" for="remember">
+                                {{ __('Remember Me') }}
+                            </label>
+                        </div>
+                    </div>
+                    <!-- /.col -->
+                    <div class="col-xs-4">
+                        <button type="submit" class="btn btn-primary btn-block btn-flat">
+                            {{ __('Login') }}
+                        </button>
+                    </div>
+                    <!-- /.col -->
+                </div>
+            </form>
+        </div>
+        <!-- /.login-box-body -->
     </div>
-</div>
+    <!-- /.login-box -->
+
+
 @endsection
